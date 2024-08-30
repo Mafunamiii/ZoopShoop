f06546be656ed4d11d438e086fb79b0871f3e02d
Jahnreil Amarillento <92299867+Mafunamiii@users.noreply.github.com>
2024-08-30 09:26:13 +0800

    feat(Database): Working database connection

--- product-catalog/product-information-service/pkg/database/database.go
+++ product-catalog/product-information-service/pkg/database/database.go
@@ -3,22 +3,30 @@ package database
 import (
 	"database/sql"
 	"fmt"
-	"github.com/rs/zerolog/log"
 	"os"
 
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
+	"github.com/rs/zerolog/log"
 )
 
 func ConnectToDatabase() (*sql.DB, error) {
 	log.Info().Msg("Connecting to the database")
+
+	// Load environment variables from .env file
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal().Err(err).Msg("Error loading .env file")
+	}
+
 	dbHost := os.Getenv("DB_HOST")
-	dbPort := os.Getenv("DB_PORT")
+	//dbPort := os.Getenv("DB_PORT")
 	dbUser := os.Getenv("DB_USER")
 	dbPassword := os.Getenv("DB_PASSWORD")
 	dbName := os.Getenv("DB_NAME")
 
-	connStr := fmt.Sprintf("host=%s port=%s dbname=%s user=%s password=%s sslmode=disable",
-		dbHost, dbPort, dbName, dbUser, dbPassword)
+	connStr := fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=require",
+		dbHost, dbUser, dbPassword, dbName)
 
 	log.Info().Msgf("Connecting to the database with connection string: %s", connStr)
 
